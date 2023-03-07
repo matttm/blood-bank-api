9537ee14401eb9a15694bd124724616f9f15e826
matttm <matttmaloney@gmail.com>
2023-03-07 08:25:07 -0500

    connecting to db

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const v1 = require('./versions/v1/v1.routes');
+const createDbInstance = require('./database');
 
 const app = express();
+const instance = await createDbInstance();
+
+try {
+    await instance.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
--- src/database/index.js
+++ src/database/index.js
@@ -1,27 +1,32 @@
 const dbConfig = require("../../db.config");
 const fs = require('fs');
-
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect
-});
 
-const db = {};
+function createDbInstance() {
+    return new Promise((res, rej) => {
+        const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+            host: dbConfig.HOST,
+            dialect: dbConfig.dialect
+        });
 
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+        const db = {};
 
-fs.readdir('./', (err, files) => {
-    if (err) {
-        console.error('Error: ', err);
-        return;
-    }
-    for (let file of files) {
-        console.log(`Loading model ${file}`);
-        const model = file.split('.')[0];
-        db[model] = require(`./${model}`)(sequelize, Sequelize);
-    }
-});
+        db.Sequelize = Sequelize;
+        db.sequelize = sequelize;
 
-module.exports = db;
+        fs.readdir('./', (err, files) => {
+            if (err) {
+                console.error('Error: ', err);
+                rej(err);
+            }
+            for (let file of files) {
+                console.log(`Loading model ${file}`);
+                const model = file.split('.')[0];
+                db[model] = require(`./${model}`)(sequelize, Sequelize);
+            }
+            res(db);
+        });
+    });
+}
+
+module.exports = createDbInstance;
