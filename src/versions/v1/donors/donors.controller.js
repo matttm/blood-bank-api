860a2dbb2e28972eb4c970d2a2b417de365d3f9e
matttm <matttmaloney@gmail.com>
2023-03-08 13:37:57 -0500

    changing aws version

--- src/versions/v1/donors/donors.controller.js
+++ src/versions/v1/donors/donors.controller.js
@@ -1,11 +1,11 @@
 const models = require('../../../database');
 // Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
+var AWS = require('@aws-sdk/client-sqs');
 // Set the region
 AWS.config.update({region: 'REGION'});
 
 // Create an SQS service object
-var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+var sqs = new AWS.SQSClient({apiVersion: '2012-11-05'});
 
 async function getDonors(req, res) {
     console.log(models);
