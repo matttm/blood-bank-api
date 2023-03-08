ade46835146decd9eb9eb97377117227db4b593f
matttm <matttmaloney@gmail.com>
2023-03-08 08:39:26 -0500

    fixing models

--- src/database/donor.model.js
+++ src/database/donor.model.js
@@ -1,16 +1,20 @@
 module.exports= (sequelize, Sequelize) => {
     return sequelize.define("Donor", {
         donorId: {
+            field: 'DONOR_ID',
             type: Sequelize.UUID,
             primaryKey: true
         },
         firstName: {
+            field: 'FIRST_NAME',
             type: Sequelize.STRING
         },
         lastName: {
+            field: 'LAST_NAME',
             type: Sequelize.STRING
         },
         bloodType: {
+            field: 'BLOOD_TYPE',
             type: Sequelize.STRING
         },
     }, {
