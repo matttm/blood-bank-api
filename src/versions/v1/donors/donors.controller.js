7e7268bf378ac6cdb73cc0540646d94413bffd20
matttm <matttmaloney@gmail.com>
2023-03-08 12:07:47 -0500

    working on queue

--- src/versions/v1/donors/donors.controller.js
+++ src/versions/v1/donors/donors.controller.js
@@ -1,5 +1,12 @@
 const models = require('../../../database');
-const obj = require("../../../database");
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region
+AWS.config.update({region: 'REGION'});
+
+// Create an SQS service object
+var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
 async function getDonors(req, res) {
     console.log(models);
     try {
@@ -28,7 +35,30 @@ async function getDonor(req, res) {
 
 async function createDonor(req, res) {
     try {
-        return models.Donor.findAll();
+        const { firstName, lastName } = req.params;
+        var params = {
+            // Remove DelaySeconds parameter and value for FIFO queues
+            DelaySeconds: 10,
+            MessageAttributes: {
+                "Title": {
+                    DataType: "String",
+                    StringValue: "The Whistler"
+                }
+            },
+            MessageBody: "New Donor Application (NDA)",
+            MessageDeduplicationId: `${firstName.toLowerCase()}-${lastName.toLowerCase()}`,  // Required for FIFO queues
+            MessageGroupId: "NDA",  // Required for FIFO queues
+            QueueUrl: process.env.SQS_QUEUE_URL
+        };
+
+        sqs.sendMessage(params, function(err, data) {
+            if (err) {
+                console.log("Error", err);
+            } else {
+                console.log("Success", data.MessageId);
+            }
+        });
+        return res.status(200).send();
     } catch (e) {
         console.error(`Error: ${e}`);
         return res.json({ success: false });
