0196328d254c533e5643ca8bf7fa8c8f11e9704e
matttm <matttmaloney@gmail.com>
2023-03-08 10:51:48 -0500

    creating donor post

--- src/versions/v1/donors/donors.controller.js
+++ src/versions/v1/donors/donors.controller.js
@@ -1,5 +1,32 @@
-const obj = require('../../../database');
+const models = require('../../../database');
+const obj = require("../../../database");
 async function getDonors(req, res) {
+    console.log(models);
+    try {
+        return models.Donor.findAll();
+    } catch (e) {
+        console.error(`Error: ${e}`);
+        throw e;
+    }
+}
+
+async function getDonor(req, res) {
+    const id = req.params['donor-id'];
+    try {
+        return res.json({
+            ...await models.Donor.findOne({
+                where: {
+                    donorId: id
+                }
+            })
+        });
+    } catch (e) {
+        console.error(`Error: ${e}`);
+        throw e;
+    }
+}
+
+async function createDonor(req, res) {
     console.log(obj);
     try {
         return obj.Donor.findAll();
@@ -8,7 +35,8 @@ async function getDonors(req, res) {
         return res.json({ success: false });
     }
 }
-
 module.exports = {
-    getDonors
+    getDonors,
+    getDonor,
+    createDonor
 };
--- src/versions/v1/donors/donors.routes.js
+++ src/versions/v1/donors/donors.routes.js
@@ -1,11 +1,14 @@
 const { Router } = require('express')
 const donorsController = require('./donors.controller');
+const {getDonor} = require("./donors.controller");
 
 const donorsRouter = Router();
 
-donorsRouter.get(
-    '/',
-    donorsController.getDonors
-);
+donorsRouter.route('/')
+    .get(donorsController.getDonors)
+    .post(donorsController.createDonor);
+
+donorsRouter.route('/:donor-id')
+    .get(getDonor);
 
 module.exports = donorsRouter;
