23c71105b414207dfad5f03ca93ba6884a8e5ae9
matttm <matttmaloney@gmail.com>
2023-03-26 07:43:07 -0400

    creating enums

---
+++ src/enums/blood=type.enum.js
@@ -0,0 +1,18 @@
+export const bloodTypeEnum = {
+    A: {
+        cd: 'A',
+        desc: ''
+    },
+    B: {
+        cd: 'B',
+        desc: ''
+    },
+    AB: {
+        cd: 'AB',
+        desc: ''
+    },
+    O: {
+        cd: 'O',
+        desc: ''
+    }
+};
