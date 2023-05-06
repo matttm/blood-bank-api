57d3aee269c9d5227d0a99528e1004af7a5767b9
matttm <matttmaloney@gmail.com>
2023-05-06 13:05:20 -0400

    adding to transaction service

--- src/versions/v1/transactions/transactions.service.js
+++ src/versions/v1/transactions/transactions.service.js
@@ -1,9 +1,46 @@
 const models = require("../../../database");
 
 function TransactionsService() {
-  async function getTransactions() {}
+  const transaction = "Transaction";
+  async function getTransactions() {
+    try {
+      return models[transaction].findAll();
+    } catch (e) {
+      console.error(e);
+      throw new Error("Error occurred while getting all transactions");
+    }
+  }
+  async function getTransaction(id) {
+    try {
+      return models[transaction].find({
+        where: {
+          transactionId: id,
+        },
+      });
+    } catch (e) {
+      console.error(e);
+      throw new Error("Error occurred while getting a transaction");
+    }
+  }
+  async function createTransaction() {
+    try {
+      return models[transaction].findAll();
+    } catch (e) {
+      console.error(e);
+      throw new Error("Error occoured while getting all transactions");
+    }
+  }
+  async function updateTransaction() {
+    try {
+      return models[transaction].findAll();
+    } catch (e) {
+      console.error(e);
+      throw new Error("Error occoured while getting all transactions");
+    }
+  }
   return Object.freeze({
     getTransactions,
+    getTransaction,
   });
 }
 
